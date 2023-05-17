1af127d263105e13ba2c08dd437df1afe27400eb
Fabio Vitello <fvitello@users.noreply.github.com>
2023-05-17 15:23:23 +0200

    Update DataTable.tsx
    Dynamically construct table

--- 2048-test/src/DataTable.tsx
+++ 2048-test/src/DataTable.tsx
@@ -1,15 +1,30 @@
 interface DataTableProps {
-    rows: number
-    columns: number
+    rows: number;
+    columns: number;
 }
 
-const DataTable:React.FC<DataTableProps> = ({rows, columns}) => {
-return(
-    <>
-        Rows:<div>{rows}</div>
-        Column:<div>{columns}</div>
-    </>
-    )
+const DataTable: React.FC<DataTableProps> = (props) => {
+    let matrix = Array(props.rows);
+    for (let i = 0; i < props.rows; i++) {
+      matrix[i] = new Array(props.columns).fill(0);
     }
+  
+    return (
+      <>
+        <table>
+        <tbody>
+          {matrix.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((column: number, columnIndex: number) => (
+                <td key={`${rowIndex}-${columnIndex}`}>{column}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      </>
+    );
+  };
 
 export default DataTable
